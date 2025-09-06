0ebca8ffc60ef2b66dc7a0ca3cca63b493e7bd19
Blame Reiker <reaperofssa@gmail.com>
2025-09-06 07:56:01 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+export const config = {
+    prefix: "!",
+    owner: "1234567890@s.whatsapp.net",
+    port: 3000,
+    mode: "development",
+    defaultBrowser: {
+        name: "Chrome",
+        os: "Ubuntu"
+    },
+    sessionTimeout: 60000,
+    keepAliveInterval: 10000
+};
